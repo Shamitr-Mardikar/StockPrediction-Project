a98ae430764128c3a59663a2c45697fcbdb99648
ShamitrM <shamitr.mardikar@gmail.com>
2024-03-30 21:24:26 +0530

    test creation

---
+++ streamlit_app.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import pandas_datareader as data
+
+start = '2010-01-01'
+end = '2020-12-31'
+
+df = data.DataReader('TSLA','yahoo',start,end)
+df.head()
